b286af19fc7decdbebdfaaa8b2b5ba768a4f968f
inchwara <inchwara@gmail.com>
2017-03-06 17:22:59 +0300

    initial commit

---
+++ primeno_tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from day1 import prime_no_solution
+
+class testprimenumbers(unittest.Testcase):
+	"""docstring for test_prime_numbers"""
+	def test_invalid(self):
+		self.assertInvalid("tracy"(Should only contain numeric values))
+
+
+
+if __name__ == '__main__':
+	unittest()
+		
+		
